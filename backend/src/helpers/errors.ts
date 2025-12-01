8ccfab590c77156ec79c7eeda0ea1e53e1b9eea0
Mario Burali <marioburali@hotmail.com>
2025-12-01 19:41:30 -0300

    fix: removing error.ts

--- backend/src/helpers/errors.ts
+++
@@ -1,9 +0,0 @@
-export const errors = {
-  200: 'OK',
-  201: 'Created',
-  400: 'Bad Request',
-  401: 'Unauthorized',
-  403: 'Forbidden',
-  404: 'Not Found',
-  500: 'Internal Server Error',
-};
